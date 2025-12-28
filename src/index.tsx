880d53556d7937a0e3f52c1f332657f152490990
Zach Kelling <z@zeekay.ai>
2025-12-28 01:29:43 -0800

    Initial commit: CodePen zOS app

---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+interface CodePenProps {
+  onClose: () => void;
+}
+
+const CodePen: React.FC<CodePenProps> = ({ onClose }) => {
+  return (
+    <div className="h-full w-full bg-[#1e1f26]">
+      <iframe
+        src="https://codepen.io/pen/"
+        className="w-full h-full border-0"
+        allow="accelerometer; camera; encrypted-media; geolocation; gyroscope; microphone"
+        title="CodePen"
+      />
+    </div>
+  );
+};
+
+export default CodePen;
